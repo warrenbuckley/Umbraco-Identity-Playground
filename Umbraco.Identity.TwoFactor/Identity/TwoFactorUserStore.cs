dbc6707ceee2b7cd4a7cde6a7037eac1a032f796
Warren Buckley <warren@creativewebspecialist.co.uk>
2015-06-19 17:51:34 +0100

    Update UserStore to check if db record exists, if not go & create one for the user

--- Umbraco.Identity.TwoFactor/Identity/TwoFactorUserStore.cs
+++ Umbraco.Identity.TwoFactor/Identity/TwoFactorUserStore.cs
@@ -11,18 +11,21 @@ namespace Umbraco.Identity.TwoFactor.Identity
     {
         public Task<bool> GetTwoFactorEnabledAsync(BackOfficeIdentityUser user)
         {
-            //Lookup db record from our custom DB table
-            //WHERE the user passed in, unique ID is the identified
-
-            //Should I user this prop on BackOfficeIdentityUser?
-            //user.TwoFactorEnabled
-
             //User ID to lookup in our table
             var userId = user.Id;
 
             //PetaPoco or nPoco that UmbracoDB uses go look it up
             var db = ApplicationContext.Current.DatabaseContext.Database;
             var record = db.SingleOrDefault<TwoFactorAuth>("WHERE userId=@0", userId);
+
+            //No record found - create one for the user
+            if (record == null)
+            {
+                var newRecord = new TwoFactorAuth {TwoFactorEnabled = false, UserId = userId};
+                db.Insert(newRecord);
+
+                return Task.FromResult(newRecord.TwoFactorEnabled);
+            }
 
             //What about creating the record initialling, is that what the othermethods are for?
             return Task.FromResult(record.TwoFactorEnabled);
@@ -30,12 +33,6 @@ namespace Umbraco.Identity.TwoFactor.Identity
 
         public Task SetTwoFactorEnabledAsync(BackOfficeIdentityUser user, bool enabled)
         {
-            //Lookup db record from our custom DB table
-            //WHERE the user passed in, unique ID is the identified
-
-            //Should I user this prop on BackOfficeIdentityUser?
-            //user.TwoFactorEnabled
-
             //User ID to lookup in our table
             var userId = user.Id;
 
@@ -44,11 +41,50 @@ namespace Umbraco.Identity.TwoFactor.Identity
 
             var record = db.SingleOrDefault<TwoFactorAuth>("WHERE userId=@0", userId);
 
+            //No record found - create one for the user
+            if (record == null)
+            {
+                var newRecord = new TwoFactorAuth { TwoFactorEnabled = false, UserId = userId };
+                db.Insert(newRecord);
+
+                return Task.FromResult(newRecord.TwoFactorEnabled);
+            }
+
             //Update the value (inverse the value)
             record.TwoFactorEnabled = !record.TwoFactorEnabled;
             db.Update(record);
 
             return Task.FromResult(0);
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task CreateAsync(BackOfficeIdentityUser user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateAsync(BackOfficeIdentityUser user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteAsync(BackOfficeIdentityUser user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<BackOfficeIdentityUser> FindByIdAsync(int userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<BackOfficeIdentityUser> FindByNameAsync(string userName)
+        {
+            throw new NotImplementedException();
         }
 
         
