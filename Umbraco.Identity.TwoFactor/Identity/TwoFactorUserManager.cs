e13d11243ff2a4991614016ca075e7160e10e629
Warren Buckley <warren@creativewebspecialist.co.uk>
2015-06-23 09:10:29 +0100

    Add in a class for userManager to set/register our TwoFactorAuthProviders. How I am not sure how I can extend the userManager as seems I can only pass in a customStore in so far.

---
+++ Umbraco.Identity.TwoFactor/Identity/TwoFactorUserManager.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNet.Identity;
+using Umbraco.Core.Models.Identity;
+using Umbraco.Core.Security;
+
+namespace Umbraco.Identity.TwoFactor.Identity
+{
+    public static class TwoFactorUserManager
+    {
+        
+        public static BackOfficeUserManager ExtendBackOfficeUserManager(this BackOfficeUserManager userManager)
+        {
+            //TODO: How do I set SupportsUserTwoFactor = true on userManager
+            //Why can I not set this?
+            //userManager.SupportsUserTwoFactor = true;
+
+            userManager.RegisterTwoFactorProvider("SMS", new PhoneNumberTokenProvider<BackOfficeIdentityUser, int>());
+            userManager.RegisterTwoFactorProvider("Email", new EmailTokenProvider<BackOfficeIdentityUser, int>());
+
+        }
+
+    }
+}
